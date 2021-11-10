24df1637bc0f87c1e73f18bd04cf80bf213f4e03
Hayley McHugh <hayleym522@gmail.com>
2021-11-09 20:50:18 -0800

    fixed bug on userAPI.js

--- utils/userAPI.js
+++ utils/userAPI.js
@@ -2,8 +2,9 @@ const axios = require('axios');
 
 const userAPI = {
     async getUser(userResponses) {
-        try {let response = await axios
-        .get('https://api.github.com/users/${userResponses.username}');
+        try { let response = await axios
+        .get(`https://api.github.com/users/${userResponses.username}`);
+        return response.data;
       
         } catch (error) {
             console.log(error);
